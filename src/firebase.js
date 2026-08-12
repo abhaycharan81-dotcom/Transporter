1ac601bbe0ce7dcbbf6318e2ab2832f1e3bae73a
abhay <abhaycharan81@gmail.com>
2026-08-12 17:54:41 +0530

    hosted as web app on firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,21 @@
-// Import the Firebase SDK functions you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase project configuration (from Firebase console)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdefg"
+  apiKey: "AIzaSyDwh8N4vwmzfkyIAmfdvGqJLOuTF9wBvOo",
+  authDomain: "transporter-b80fb.firebaseapp.com",
+  projectId: "transporter-b80fb",
+  storageBucket: "transporter-b80fb.firebasestorage.app",
+  messagingSenderId: "373790816073",
+  appId: "1:373790816073:web:56725abfbb04139a7ecc98",
+  measurementId: "G-E9VNK9EDPX"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export services for use in your app
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
