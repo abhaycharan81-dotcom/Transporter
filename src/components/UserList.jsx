c124d896416bfcb88cf73d4c1c115e4d901d0114
abhay <abhaycharan81@gmail.com>
2026-08-12 18:55:46 +0530

    adding database

---
+++ src/components/UserList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+
+function UsersList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      setUsers(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name} ({user.email})</li>
+      ))}
+    </ul>
+  );
+}
+
+export default UsersList;
